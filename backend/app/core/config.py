56309cd465b06fe20b605bff55f3916c5d5beece
mfecproject01-maker <mfec.project01@gmail.com>
2026-06-04 13:39:19 +0700

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -26,6 +26,8 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5500",
         "http://127.0.0.1:5501",
         "null",
+        "https://admin-console-batool-6a2wlixh1-mfecproject01-makers-projects.vercel.app",
+        "https://ba-tool-4bkpuk5mj-mfecproject01-makers-projects.vercel.app",
     ]
     ALLOWED_ORIGIN_REGEX: Optional[str] = (
         r"https://admin-console-for-batool(-[a-z0-9]+)?\.vercel\.app"
